e01f0a3a8a59ff05b19ceb67fa6d20e8feb8f1de
rolyan66@gmail.com <rolyan66@gmail.com>
2025-09-09 14:28:33 -0400

    Se realizo la creacion de la estructura del proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GastosComponent } from './components/gastos/gastos.component';
+import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
+import { IngresarGastoComponent } from './components/gastos/ingresar-gasto/ingresar-gasto.component';
+import { ListarGastoComponent } from './components/gastos/listar-gasto/listar-gasto.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GastosComponent,
+    IngresarPresupuestoComponent,
+    IngresarGastoComponent,
+    ListarGastoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
