26878819863b0cc09883d0d757d4b8497edb0b94
rolyan66@gmail.com <rolyan66@gmail.com>
2025-09-12 16:30:27 -0400

    Se realizo los cambis de front

--- src/app/components/gastos/listar-gasto/listar-gasto.component.ts
+++ src/app/components/gastos/listar-gasto/listar-gasto.component.ts
@@ -11,11 +11,13 @@ export class ListarGastoComponent implements OnDestroy,OnInit {
   subscription:Subscription;
   presupuesto:number;
   restante:number;
+  listGastos:any[]=[];
   constructor(private _presupuestoService:PresupuestoService){
     this.presupuesto=0;
     this.restante=0
     this.subscription=this._presupuestoService.getGastos().subscribe(data =>{
-      console.log(data);
+      this.restante=this.restante - data.cantidad
+      this.listGastos.push(data);
     });
   }
   ngOnInit(): void {
@@ -24,5 +26,15 @@ export class ListarGastoComponent implements OnDestroy,OnInit {
   }
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+  }
+  aplicarColorRestante(){
+    if(this.presupuesto / 4 > this.restante)
+    {
+      return 'alert alert-danger'
+    }else if(this.presupuesto / 2 > this.restante){
+      return 'alert alert-warning';
+    }else{
+      return 'alert alert-secondary';
+    }
   }
 }
