c95006c2a4686df167308e574799a916e41ee04f
rolyan66@gmail.com <rolyan66@gmail.com>
2025-09-12 15:22:55 -0400

    Se realizo la incorporacion de diseño de listado de gastos

--- src/app/components/gastos/listar-gasto/listar-gasto.component.ts
+++ src/app/components/gastos/listar-gasto/listar-gasto.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { PresupuestoService } from 'src/app/services/presupuesto.service';
 
 @Component({
@@ -6,11 +7,22 @@ import { PresupuestoService } from 'src/app/services/presupuesto.service';
   templateUrl: './listar-gasto.component.html',
   styleUrls: ['./listar-gasto.component.css']
 })
-export class ListarGastoComponent {
+export class ListarGastoComponent implements OnDestroy,OnInit {
+  subscription:Subscription;
+  presupuesto:number;
+  restante:number;
   constructor(private _presupuestoService:PresupuestoService){
-    this._presupuestoService.getGastos().subscribe(data =>{
+    this.presupuesto=0;
+    this.restante=0
+    this.subscription=this._presupuestoService.getGastos().subscribe(data =>{
       console.log(data);
     });
   }
-
+  ngOnInit(): void {
+    this.presupuesto=this._presupuestoService.presupuesto;
+    this.restante=this._presupuestoService.restante;
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
